49b075a636361a4ad46b14f6a1eb5d86475a9c6d
Kealoha1988 <kealohaabe1988@gmail.com>
2021-01-07 15:28:39 -0500

    problem with delete

--- app/controllers/bag_controller.rb
+++ app/controllers/bag_controller.rb
@@ -12,6 +12,11 @@ class BagController < ApplicationController
 
     erb :'bag/index'
   end
+
+  get "/bag/:id/edit" do
+    @bag = Bag.find_by_id(params[:id])
+    erb :'bag/edit'
+  end
   
 
 
@@ -26,6 +31,29 @@ class BagController < ApplicationController
   
     erb :'bag/show'
   end
+  
+  patch '/bag/:id' do
+    @bag = Bag.find_by_id(params[:id])
+    redirect '/bag' unless @bag
+    if @bag.update(params[:bag])
+        redirect "/bag/#{@bag.id}"
+    else
+        redirect "/bag/#{@bag.id}/edit"
+    end
+
+    delete '/bag/3' do
+      "Hello World"
+    end
+
+    private
+
+    def find_bag
+      @bag = Bag.find_by_id(params[:id])
+    end
+
+
+end
+
 
 
 
