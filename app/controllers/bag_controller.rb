b7dec5f48677092f627d49b052fc1042ee1b0277
Kealoha1988 <kealohaabe1988@gmail.com>
2021-01-07 11:44:19 -0500

    before the change

--- app/controllers/bag_controller.rb
+++ app/controllers/bag_controller.rb
@@ -1,30 +1,35 @@
 class BagController < ApplicationController
 
   
+  
   get '/bag/new' do
     erb :'bag/new'
   end
 
 
-
   get '/bag' do
     @bags = Bag.all
+  
 
     erb :'bag/index'
   end
+  
+
 
   post '/bag/made' do
-    erb :'bag/made'
-  end
-
-  post '/bag' do
     bag = Bag.new(params[:bag])
     bag.save
     erb :'bag/made'
   end
+  
+  get '/bag/:id' do
+    @bag = Bag.find_by_id(params[:id])
+   
+    erb :'bag/show'
+  end
 
-  delete '/bag/delete' do
-  @bags[id].clear
+  get '/bag/delete' do
+  
 end
 
 
