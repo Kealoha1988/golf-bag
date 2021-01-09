3782b900fe604c05ffead0259b80b906b50f0e1a
Kealoha1988 <kealohaabe1988@gmail.com>
2021-01-09 16:51:32 -0500

    change for name

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -16,6 +16,21 @@ class UserController < ApplicationController
     end
   end
 
+  get '/user/:id' do
+    @user = current_user
+    erb :'user/edit'
+  end
+
+  patch '/user/:id' do
+    @user = current_user
+    redirect '/user' unless @user
+    if @user.update(params[:user])
+      @user = @user.update(params[:user])
+        redirect "/user/#{@user.id}"
+    else
+        redirect "/"
+    end 
+  end
 
 
   delete '/user/:id' do
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210109163617) do
+ActiveRecord::Schema.define(version: 20210109202045) do
 
   create_table "bags", force: :cascade do |t|
     t.string   "name"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20210109163617) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.text     "bio"
   end
 
 end
